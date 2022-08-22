bcd5780338fd47d0d0a86ea3c9afefe82e504821
ismilearefin <arefinismile@gmail.com>
2022-08-22 17:40:13 +0600

    js second part done

--- js/assignmnet.js
+++ js/assignmnet.js
@@ -3,24 +3,36 @@ let total = [];
 
 function selectedPlayer(clicked){
   
-      const playerName = clicked.parentNode.children[1].innerText ;
-      total.push(playerName);
-      
-      if(total.length<6){
-        const selectedList = document.getElementById('selected-V');
-    
-      const listIteam = document.createElement('li');
-      
-        listIteam.innerHTML = `
-        ${playerName}
-        `
-        selectedList.appendChild(listIteam);
-      }else{
-        const elemnts = document.getElementsByTagName('button');
-        for(let elemet of elemnts){
-          elemet.style.background = 'gray';
-          elemet.classList.add ('cursor-not-allowed');
-        }
-      }
+  const playerName = clicked.parentNode.children[1].innerText ;
+  total.push(playerName);
+  
+  if(total.length<6){
+    const selectedList = document.getElementById('selected-V');
+
+  const listIteam = document.createElement('li');
+  
+    listIteam.innerHTML = `
+    ${playerName}
+    `
+    selectedList.appendChild(listIteam);
+  }else{
+    const elemnts = document.getElementsByTagName('button');
+    for(let elemet of elemnts){
+      elemet.style.background = 'gray';
+      elemet.classList.add ('cursor-not-allowed');
+    }
+  }
+  
 }
 
+document.getElementById('calculateBtn').addEventListener('click', function(){
+
+  let totalCost = document.getElementById('totalExpense');
+  totalCost.innerText = costCalculte(total.length);
+})
+
+document.getElementById('calculateTotal').addEventListener('click', function(){
+  let totalCost = document.getElementById('totalExpense');
+  let finalCost = document.getElementById('total');
+  finalCost.innerText = parseInt(totalCost.innerText) + othersCost();
+})
--- js/extra.js
+++ js/extra.js
@@ -1,3 +1,13 @@
 function costCalculte(num){
- 
+  const playerCost =  document.getElementById('perPlayerCost');
+  let result =  num * parseInt(playerCost.value);
+
+return result ;
+}
+
+function othersCost (){
+  const manager =  document.getElementById('managerCost');
+  const coach =  document.getElementById('coachCost');
+  let totalcost = parseInt(manager.value) + parseInt(coach.value);
+  return totalcost;
 }
