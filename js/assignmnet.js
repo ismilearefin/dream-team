51be3a66704fb57a8e4b99dd0db7a2c41734667d
ismilearefin <arefinismile@gmail.com>
2022-08-22 17:07:13 +0600

    first part js done

--- js/assignmnet.js
+++ js/assignmnet.js
@@ -1,1 +1,26 @@
-console.log('fahad');
+
+let total = [];
+
+function selectedPlayer(clicked){
+  
+      const playerName = clicked.parentNode.children[1].innerText ;
+      total.push(playerName);
+      
+      if(total.length<6){
+        const selectedList = document.getElementById('selected-V');
+    
+      const listIteam = document.createElement('li');
+      
+        listIteam.innerHTML = `
+        ${playerName}
+        `
+        selectedList.appendChild(listIteam);
+      }else{
+        const elemnts = document.getElementsByTagName('button');
+        for(let elemet of elemnts){
+          elemet.style.background = 'gray';
+          elemet.classList.add ('cursor-not-allowed');
+        }
+      }
+}
+
